d6c580a1b626db2e0f512fe9e9cee3afe11b1307
heriksevak <msevak4@gmail.com>
2025-09-21 10:27:55 +0530

    Initial portfolio commit 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}", // This looks for HTML and JS files in the root folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
